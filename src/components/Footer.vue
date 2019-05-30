9c5bdee8ae646d655f722b088ef0939deffd9f39
Dmitry Vakin <dmitry.vakin.id@gmail.com>
2019-05-31 00:33:20 +0300

    Added footer and nav components

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Thea</strong> 2019
+      </p>
+    </div>
+  </footer>
+</template>
+<script lang='ts'>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Footer extends Vue {}
+</script>
+<style lang='scss'>
+</style>
